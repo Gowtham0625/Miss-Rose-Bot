e1841a4ed6ee366442c59dd2fc24433044d6e8e3
Fᴀʏᴀs.N <73426323+FayasKKD@users.noreply.github.com>
2020-11-28 10:42:22 +0530

    Update muting.py

--- tg_bot/modules/muting.py
+++ tg_bot/modules/muting.py
@@ -184,7 +184,7 @@ __help__ = """
  - /unmute <userhandle>: unmutes a user. Can also be used as a reply, muting the replied to user.
 """
 
-__mod_name__ = "Mute"
+__mod_name__ = "Mutings"
 
 MUTE_HANDLER = CommandHandler("mute", mute, pass_args=True, filters=Filters.group)
 UNMUTE_HANDLER = CommandHandler("unmute", unmute, pass_args=True, filters=Filters.group)
