408e9583676f190f8b32c80c2885cea6c0787a03
Shirelle Chalamish <shirelle.chalamish@gmail.com>
2021-09-25 20:32:55 -0700

    Update config.py

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -25,6 +25,7 @@ class Development(Config):
     PORT = 5000
     DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
     STRICT_GBAN = False
+    STRICT_GMUTE = False
     WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
     BAN_STICKER = 'CAADAgADOwADPPEcAXkko5EB3YGYAg'  # banhammer marie sticker
     ALLOW_EXCL = False  # Allow ! commands as well as /
